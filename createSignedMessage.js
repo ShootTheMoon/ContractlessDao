aee766158878d8622bf8185553ac73e77a993b50
Daniel Odom <daniel@paladin.lab>
2023-11-07 18:58:37 -0700

    add request sender

---
+++ createSignedMessage.js
@@ -0,0 +1,23 @@
+const {ethers} = require('ethers');
+const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
+
+const wallet = ethers.Wallet.createRandom();
+
+async function main(){
+  const vote = `${uuidv4()}.01.${uuidv4()}`;
+  const signedMessage = await wallet.signMessage(vote);
+    
+  console.log(signedMessage, wallet.address, vote);
+
+  const res = await axios.post('http://localhost:3000/vote/cast', {
+    vote: vote,
+    walletAddress: wallet.address,
+    voteSignature: signedMessage
+  });
+
+  console.log(res.data);
+}
+
+main();
+
