7c8e15bde38cd304e5cc06b3e975889aa7409a77
Daniel Odom <daniel@paladin.lab>
2023-11-07 18:58:59 -0700

    route fix and add mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -6,7 +8,20 @@ app.use(express.json());
 
 import {router as voteRouter} from './routes/vote.routes';
 
-app.post('/vote', voteRouter);
+import mongoose from 'mongoose';
+
+const {ENV, MONGODB_PASSWORD, MONGODB_SERVER, MONGODB_USERNAME, DATABASE_PRODUCTION, DATABASE_TEST} = process.env;
+
+if (ENV === 'development') {
+  mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_SERVER}/${DATABASE_TEST}`);
+}
+
+if (ENV === 'production') {
+  mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_SERVER}/${DATABASE_PRODUCTION}`);
+}
+
+
+app.use('/vote', voteRouter);
 
 app.listen(3000, () => {
   console.log('Server running');
